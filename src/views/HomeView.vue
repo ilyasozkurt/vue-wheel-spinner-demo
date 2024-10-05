ef4d427af56e693c9b647f7363d01da309036a3d
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2024-10-05 02:02:28 +0000

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,7 +34,7 @@
                   <h1 class="card-title text-primary">🎉 {{ winnerResult.text }} 🎉</h1>
                    <br>
                   <button type="button" class="btn mx-auto btn-primary text-white">
-                    <RouterLink to="/services/randomnamepicker/uniqueidentifier/xyz9876543/generator/chooseyourwinner/selectrandomly/satu" class="text-white">Simpan & Putar Kembali</RouterLink>
+                    <RouterLink to="/services/randomnamepicker/uniqueidentifier/xyz9876543/generator/chooseyourwinner/selectrandomly/dua" class="text-white">Simpan & Putar Kembali</RouterLink>
                   </button>
                 </div>
               </div>
